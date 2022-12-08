cba7b29d13e92801e7486baab2763ef949156493
D-a-r-i-u-s <darius.valiuss@gmail.com>
2022-12-08 19:52:58 +0200

    Ketvirta uzduotis

--- JS Tasks/Task 4/script.js
+++ JS Tasks/Task 4/script.js
@@ -8,4 +8,33 @@ Pastaba: Sukurta kortelė, kurioje yra informacija apie automobilį (brand), tur
 būti stilizuota su CSS ir būti responsive;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'cars.json';
+// const ENDPOINT = 'cars.json';
+
+async function getData() {
+    try {
+        const response = await fetch('./cars.json');
+        if (response.ok) {
+            const data = await response.json();
+            renderData(data);
+            console.log(data);
+        }
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+const renderData = (data) => {
+    const output = document.querySelector('#output');
+    let postData = '';
+    data.forEach(post => {
+        postData += `<div class="card">
+                        <h2 class="brand">${post.brand}</h2>                        
+                        <p class="models">${post.models.join(", ")}</p>   
+                                         
+                    </div>`
+    })
+    output.innerHTML = postData;
+}
+
+getData();
